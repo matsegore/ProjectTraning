09d250aa9ca3243a37682fdbb69503cf9a00c1e5
Aleksandr Matsegora <a.matsegora@intetics.com>
2019-03-30 13:57:58 +0300

    Square calculation

--- ProjectTraning/Program.cs
+++ ProjectTraning/Program.cs
@@ -10,6 +10,21 @@ namespace ProjectTraning
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Введите радиус");
+            string radiusString = Console.ReadLine();
+            int.TryParse(radiusString, out int radius);
+            Console.WriteLine($"The square of the round {Program.PerformCalculation(radius)}");
+        }
+
+
+        public static decimal PerformCalculation(int r)
+        {
+            const decimal Pi = 3.14m;
+
+            decimal square = Pi * r * r;
+
+            return square;
         }
     }
 }

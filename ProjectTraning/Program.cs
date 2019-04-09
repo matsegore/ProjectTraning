6269b6d75780c77174f1a5aa972c424c335e1612
Aleksandr Matsegora <a.matsegora@intetics.com>
2019-04-09 10:08:02 +0300

    HomeTask 1

--- ProjectTraning/Program.cs
+++ ProjectTraning/Program.cs
@@ -10,21 +10,46 @@ namespace ProjectTraning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            Console.WriteLine("Введите радиус");
-            string radiusString = Console.ReadLine();
-            int.TryParse(radiusString, out int radius);
-            Console.WriteLine($"The square of the round {Program.PerformCalculation(radius)}");
+
+            Task1();
+            //Task5();
+            //Task5t();
+            //Task6();
         }
-
-
-        public static decimal PerformCalculation(int r)
+        static void Task1()
         {
-            const decimal Pi = 3.14m;
+            Console.WriteLine("Add number");
+            string str = Console.ReadLine();
+            int a = Convert.ToInt32(str);
+            int n;
+            int converting;
+            string newNumb = "";
+            Console.WriteLine("Chuse number for del");
+            int del = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < str.Length; i++)
+            {
+                n = a % 10;
+                a /= 10;
+                if (n != del)
+                {
 
-            decimal square = Pi * r * r;
+                    newNumb += n.ToString();
 
-            return square;
+                }
+            }
+            int temp = 0;
+            converting = Convert.ToInt32(newNumb);
+            while (converting > 0)
+            {
+                temp *= 10;
+                temp += converting % 10;
+                converting /= 10;
+            }
+            Console.WriteLine(temp);
+
+
+
         }
+
     }
 }

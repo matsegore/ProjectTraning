2045bc76192915792f993d66b698b3c5eef94bd4
Aleksandr Matsegora <a.matsegora@intetics.com>
2019-04-16 19:04:32 +0300

    Task3_16.04

--- ProjectTraning/Program.cs
+++ ProjectTraning/Program.cs
@@ -10,7 +10,9 @@ namespace ProjectTraning
     {
         static void Main(string[] args)
         {
-            Task1();
+            //Task1();
+            //Task2();
+            Task3();
         }
         public static void Task1()
         {
@@ -20,8 +22,8 @@ namespace ProjectTraning
             var temp = new Random();
 
             Console.WriteLine("Заполните значениями массив");
-            for (int i=0; i<array.Length; i++)
-            {          
+            for (int i = 0; i < array.Length; i++)
+            {
                 array[i] = temp.Next(0, 1000);
                 Console.WriteLine($"Значение массива {array[i]}");
             }
@@ -48,9 +50,9 @@ namespace ProjectTraning
                 sum += array[i];
             }
             Console.WriteLine($"Сумма чисел: {sum}");
-           
-                sum /= array.Length;
-           
+
+            sum /= array.Length;
+
             Console.WriteLine($"Среднее Арифметическое: {sum}");
             Console.WriteLine($"Нечетные числа: ");
             for (int i = 0; i < array.Length; i++)
@@ -61,6 +63,71 @@ namespace ProjectTraning
                 }
             }
         }
+        public static void Task2()
+        {
 
+
+            int[] array = new int[] { 5, 2, 3, 5, 8, 5 };
+            int[] arrayWithoutDup = new int[6];
+            arrayWithoutDup[0] = array[0];
+            bool isDuplikate = false;
+            int position = 1;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = 0; j < position + 1; j++)
+                {
+                    if (array[i] == arrayWithoutDup[j])
+                    {
+                        isDuplikate = true;
+                        break;
+                    }
+                    if (!isDuplikate)
+                    {
+                        arrayWithoutDup[position] = array[i];
+                        position++;
+                    }
+                        isDuplikate = false;
+                        
+                    }
+               
+                }
+            foreach (var item in arrayWithoutDup)
+            {
+                Console.WriteLine(arrayWithoutDup[item]);
+            }
+
+        }
+        public static void Task3()
+        {
+            Console.WriteLine("Add max");
+            int max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add min");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add array length");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[a];
+            Console.WriteLine("Заполните значениями массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Add number: ");
+                array[i] = Int32.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i< array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    Console.WriteLine($"Bigger than max: {array[i]}");
+                }
+                 else if (min > array[i])
+                {
+                    Console.WriteLine($"Less than min: {array[i]}");
+                }
+            }
+        }
     }
-}
+    }
+    
+
+
+

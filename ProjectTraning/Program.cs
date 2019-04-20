4b033c16198ab36e653c3c44570bbbec7a587f83
Aleksandr Matsegora <a.matsegora@intetics.com>
2019-04-20 11:50:25 +0300

    Task1fixes

--- ProjectTraning/Program.cs
+++ ProjectTraning/Program.cs
@@ -10,41 +10,8 @@ namespace ProjectTraning
     {
         static void Main(string[] args)
         {
-            Task1();
-        }
-        public static void Task1()
-        {
-            Console.WriteLine("Add first number: ");
-            int firstArgiment = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Add second number: ");
-            int secondArgument = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Chuse operation:");
-            string symbol = Console.ReadLine();
+            Task_1.Task1();
 
-        }
-
-        private static int Add(int firstnumb, int secondnumb)
-        {
-            int adding = firstnumb + secondnumb;
-            return adding;
-        }
-
-        private static int Sub(int firstnumb, int secondnumb)
-        {
-            int subtr = firstnumb - secondnumb;
-            return subtr;
-        }
-
-        private static int Mul(int firstnumb, int secondnumb)
-        {
-            int mult = firstnumb * secondnumb;
-            return mult;
-        }
-
-        private static int Div(int firstnumb, int secondnumb)
-        {
-            int div = firstnumb * secondnumb;
-            return div;
-        }
+        }      
     }
 }
---
+++ ProjectTraning/Task_1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTraning
+{
+    class Task_1
+    {
+        public static void Task1()
+        {
+            Console.WriteLine("Add first number: ");
+            double firstArgiment = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add second number: ");
+            double secondArgument = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Chuse operation:");
+            string symbol = Console.ReadLine();
+            Console.WriteLine (ShowResult (firstArgiment, secondArgument, symbol));
+            
+        }
+
+        public static double WhitchSign (double firstnumb, double secondnumb, string symbol)
+        {
+            double result =0;
+            switch (symbol)
+            {
+
+                case "+":
+                    result = Task_1.Add(firstnumb, secondnumb);
+                    break;
+                case "-":
+                    result =  Sub(firstnumb, secondnumb);
+                    break;
+                case "/":
+                    if (firstnumb == 0 || secondnumb == 0)
+                    {
+                        Console.WriteLine("Error");
+                    }
+                    else
+                    {
+                        result = Div(firstnumb, secondnumb);
+                    }
+                    break;
+
+                case "*":
+                    result = Mul(firstnumb, secondnumb);
+                    break;
+                default:
+                    Console.WriteLine("Invalid sign");
+                    break;
+                    
+            }
+            return result;
+        }
+
+        private static double Add(double firstnumb, double secondnumb)
+        {
+            return firstnumb + secondnumb;
+        }
+
+        private static double Sub(double firstnumb, double secondnumb)
+        {
+            return firstnumb - secondnumb;
+        }
+
+        private static double Mul(double firstnumb, double secondnumb)
+        {
+            return firstnumb * secondnumb;
+        }
+
+        private static double Div(double firstnumb, double secondnumb)
+        {
+            return firstnumb * secondnumb;
+        }
+
+        private static double ShowResult (double firstnumb, double secondnumb, string symbol)
+        {
+            return WhitchSign(firstnumb, secondnumb, symbol);
+
+
+        }
+    }
+}
+
+

04d5faad8b6ea339c0f8f60563354dac556f351c
Aleksandr Matsegora <a.matsegora@intetics.com>
2019-06-15 13:10:56 +0300

    1

---
+++ Paract.15_06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Paract._15_06.Task1;
+namespace Paract._15_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var date1 = DateTime.Today;
+            //Console.WriteLine(date1.ToString("MM.dd.yyy"));
+
+            //var yesterday = date1.AddDays(-1);
+            //Console.WriteLine(yesterday);
+
+            //var subMonth = date1.AddMonths(-2);
+            //Console.WriteLine(subMonth);
+
+            //var subYear = date1.AddYears(-1);
+            //Console.WriteLine(subYear);
+
+            //var birthDate = new DateTime(1994, 5, 16);
+            //Console.WriteLine(birthDate);
+            MaList<int> list = new MaList<int>();
+        }
+    }
+}
---
+++ Paract.15_06/Task1/MaList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paract._15_06.Task1
+{
+    public class MaList<T> : IMyList<T>
+    {
+        private T[] array;
+
+        public int Count => this.array.Length;
+
+        T IMyList<T>.this[int index]
+        {
+            get
+            {
+                if (index > array.Length - 1)
+                {
+                    throw new Exception("Index is out of range");
+                }
+                else
+                {
+                    return array[index];
+                }
+            }
+
+        }
+
+        public MaList(T[] array)
+        {
+            ;
+        }
+
+        public void Add(T item)
+        {
+            T[] array2 = new T [array.Length+1];
+            array.CopyTo(array2.Get);
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

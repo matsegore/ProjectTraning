91e85a7e966caccf24aebcd3008fbd86b4822963
Aleksandr Matsegora <a.matsegora@intetics.com>
2019-06-15 11:08:56 +0300

    Task1

---
+++ Pract08_06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pract08_06.Task1;
+
+namespace Pract08_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new FolderPath().Calc();
+        }
+    }
+}
---
+++ Pract08_06/Task2/CreateFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Pract08_06.Task2
+{
+    class CreateFile
+    {
+        private const string Path = @"C:\Temp\userText.txt";
+
+        static FileInfo file = new FileInfo(Path);
+
+        static FileStream files = file.Create();
+
+        StreamWriter fs = new StreamWriter(files);
+
+        public 
+    }
+}

46f8776eacef3329067d8c74a0b52047fd253c07
Aleksandr Matsegora <a.matsegora@intetics.com>
2019-09-01 17:41:56 +0300

    Hunter

---
+++ ProjectTraning/Hunter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTraning
+{
+    class Hunter
+    {
+        public void PrintRabbitLocation(int coordinateX, int coordinateY)
+        {
+            Console.WriteLine($"Rabbit was catched on coordinates{Environment.NewLine}" +
+
+                $"X: {coordinateX}{Environment.NewLine}" +
+
+                $"Y: {coordinateY}{Environment.NewLine}");
+        }
+    }
+}
---
+++ ProjectTraning/Rabbit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProjectTraning
+{
+    class Rabbit
+    {
+        public delegate void LocationDelegate(object sender, HuntEventArgs args);
+
+        public event LocationDelegate EventNewLocation;
+
+        public int FirstCoordinate { get; set; }
+
+        public int SecondCoordinate { get; set; }
+
+        public void ChangeLocation()
+        {
+            var random = new Random();
+
+            while (true)
+            {
+                this.FirstCoordinate = random.Next();
+
+                this.SecondCoordinate = random.Next();
+
+                this.OnEventNewLocation(new HuntEventArgs(this.FirstCoordinate, this.SecondCoordinate));
+
+                Thread.Sleep(2000);
+            }
+        }
+
+        protected virtual void OnEventNewLocation(HuntEventArgs e)
+        {
+            this.EventNewLocation?.Invoke(this, e);
+        }
+    }
+}

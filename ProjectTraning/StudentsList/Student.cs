6810b1c66ae761f4b45cf5e8ca9262cc50a48daf
Aleksandr Matsegora <a.matsegora@intetics.com>
2019-09-01 13:17:12 +0300

    StudentList

---
+++ ProjectTraning/StudentsList/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTraning.StudentList
+{
+    class Student
+    {
+        private string firstName;
+
+        private string lastName;
+
+        public int Course { get; set; }
+
+        public string Group { get; set; }
+
+        public string Faculty { get; set; }
+
+        public string FirstName
+        {
+            get { return firstName; }
+
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+
+            set { lastName = value; }
+        }
+
+        public Student(string firstName, string lastName, int course, string group, string faculty)
+        {
+            this.FirstName = firstName;
+
+            this.LastName = lastName;
+
+            this.Course = course;
+
+            this.Group = group;
+
+            this.Faculty = faculty;
+        }
+
+        public override string ToString()
+        {
+            return this.FirstName + " " + this.LastName + ", " + this.Course + ", " + this.Group + ", " + this.Faculty;
+        }
+    }
+}
---
+++ ProjectTraning/StudentsList/StudentList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTraning.StudentList
+{
+    class StudentList
+    {
+        private List<Student> studentList = new List<Student> {
+
+            new Student ("Adam", "Potter", 4, "Z67", "Сhemistry"),
+
+            new Student ("Alex", "Smith", 3, "L11", "Math"),
+
+            new Student ("James", "Bond", 2, "P43", "Med")
+        };
+
+        public List<Student> GetStudentList()
+        {
+            return studentList;
+        }
+    }
+}

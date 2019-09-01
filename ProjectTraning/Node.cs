94a42b4006cca86d71992a33c933e968a8e33a86
Aleksandr Matsegora <a.matsegora@intetics.com>
2019-09-01 17:59:21 +0300

    linlist

---
+++ ProjectTraning/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTraning
+{
+    public class Node<T>
+    {
+        public T Element { get; set; }
+
+        public Node<T> Next { get; set; }
+
+        public Node(T element)
+        {
+            this.Element = element;
+        }
+    }
+}
+
